96f5ee7972a456281fb96c8fa53797ee85683931
Premium07 <premsagarg23@gmail.com>
2024-11-20 14:30:47 +0545

    create book modal

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import globalErrorHandler from "./middlewares/globalErrorHandler";
 import userRouter from "./user/userRouter";
+import bookRouter from "./book/bookRouter";
 
 const app = express();
 
@@ -12,8 +13,8 @@ app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res, next) => {
   res.json({ message: "Hello, this is ebook lib API" });
 });
-
 app.use("/api/users", userRouter);
+app.use("/api/books", bookRouter);
 
 // Global error handler
 app.use(globalErrorHandler);
