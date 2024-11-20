31960021f4bcba8c8b8693300197835777806e5f
Premium07 <premsagarg23@gmail.com>
2024-11-20 15:35:03 +0545

    setup multer

--- src/book/bookController.ts
+++ src/book/bookController.ts
@@ -1,12 +1,9 @@
 import { NextFunction, Request, Response } from "express";
 
-const createBook = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-    
-    res.json({message:"OK"})
+const createBook = async (req: Request, res: Response, next: NextFunction) => {
+  const {title, description, genre, author} = req.body;
+  
+  res.json({ message: "OK" });
 };
 
 export { createBook };
